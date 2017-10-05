c3d5b19aa2f657cd40183258e9cbbe2710aee6e4
Saurabh Mirajkar <mirajkar46@gmail.com>
2017-10-05 10:50:42 +0530

    Just trying to commit

---
+++ sample.swift
@@ -0,0 +1,18 @@
+func checkName(personName name: String, password pass: Int) -> String {
+	return "\(name)! Your password is \(pass)"
+}
+
+var isValid:Bool = false
+
+if name = "Saurabh" && password = 10 {
+	isValid = true
+	print("Hello \(name)")
+	while isValid {
+		print("You gotta skills")
+		isValid = false
+	}
+}else {
+	print("Something went wrong!!")
+}
+
+checkName(personName = "Saurabh", password = 10)
